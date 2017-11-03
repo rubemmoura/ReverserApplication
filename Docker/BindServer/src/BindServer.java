85c2d0a422022075bcb8b7bd67c5957141c9a31c
Rubem Moura <rfmj2@cin.ufpe.br>
2017-11-02 21:43:10 -0300

    Adicionando endereço ip correto. Endereço ip mostrado atraves do comando docker inspect ID_CONTAINER

--- Docker/BindServer/src/BindServer.java
+++ Docker/BindServer/src/BindServer.java
@@ -8,8 +8,8 @@ public class BindServer {
 		// register reverser in Naming service
 		ReverserProxy reverser = new ReverserProxy("127.0.0.1",1314);
 		
-		NamingProxy namingService = new NamingProxy("127.0.0.1", 1313);
+		NamingProxy namingService = new NamingProxy("172.17.0.2", 1313);
 		namingService.bind("Reverser", reverser);
-		System.out.println("Servidor da aplicacao foi adicionado ao serviço de nomes.");
+		System.out.println("Servidor da aplicacao foi adicionado ao servico de nomes.");
 	}
 }
