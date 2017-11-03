147e797bc7f4efea49c37635334876e48b9f8d02
Rubem Moura <rfmj2@cin.ufpe.br>
2017-11-03 15:28:56 -0300

    Ajustando endereço dos hosts para serem executados como Microserviços. Total de 5 microservices, 1 com finalidade rapida.

--- Docker/BindServer/src/BindServer.java
+++ Docker/BindServer/src/BindServer.java
@@ -6,9 +6,9 @@ public class BindServer {
 		// remote object
 		// obtain instance of Naming Service
 		// register reverser in Naming service
-		ReverserProxy reverser = new ReverserProxy("127.0.0.1",1314);
+		ReverserProxy reverser = new ReverserProxy("172.17.0.3",1314);
 		
-		NamingProxy namingService = new NamingProxy("127.0.0.1", 1313);
+		NamingProxy namingService = new NamingProxy("172.17.0.2", 1313);
 		namingService.bind("Reverser", reverser);
 		System.out.println("Servidor da aplicacao foi adicionado ao servico de nomes.");
 	}
--- Docker/ReverserClient/src/ReverserClient.java
+++ Docker/ReverserClient/src/ReverserClient.java
@@ -6,16 +6,15 @@ public class ReverserClient {
 	public static void main(String[] args) throws UnknownHostException, IOException, Throwable {
 		
 		// create an instance of Naming Service
-		NamingProxy namingService = new NamingProxy("127.0.0.1", 1313);
+		NamingProxy namingService = new NamingProxy("172.17.0.2", 1313);
 		
 		// look for Reverser in Naming service
-		// ReverserProxy reverserProxy = (ReverserProxy) namingService.lookup("Reverser");
-		ProxyClientProxy clientProxy = new ProxyClientProxy("127.0.0.1", 1315);
+		ProxyClientProxy clientProxy = new ProxyClientProxy("172.17.0.4", 1315);
 		ReverserProxy reverserProxy = clientProxy.proxy(namingService);
 		
 		// invoke reverser		
 		StringBuffer str = new StringBuffer("Programacao concorrente Distribuida - Midlleware");
-		for(int i = 0; i<10000; i++) {
+		for(int i = 0; i<5000; i++) {
 			str = reverserProxy.inverter(str);
 			System.out.println(i + " " + str);
 			Thread.sleep(5);
