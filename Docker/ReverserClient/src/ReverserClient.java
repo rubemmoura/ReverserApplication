a2f4cea304baf892ae1e0bf1489d12b145ea0daf
Rubem Moura <rfmj2@cin.ufpe.br>
2017-11-22 20:19:01 -0300

    adicionando parametros de avaliação de tempo

--- Docker/ReverserClient/src/ReverserClient.java
+++ Docker/ReverserClient/src/ReverserClient.java
@@ -12,12 +12,17 @@ public class ReverserClient {
 		ProxyClientProxy clientProxy = new ProxyClientProxy("172.17.0.6", 1315);
 		BindServerProxy reverserProxy = clientProxy.proxy(namingService,0);
 
-		// invoke reverser		
+		// invoke reverser
+		float time1, time2 = 0;
+
+		time1 = System.nanoTime();
 		StringBuffer str = new StringBuffer("Programacao concorrente Distribuida - Midlleware");
-		for(int i = 0; i<100; i++) {
+		for(int i = 0; i<50000; i++) {
 			str = reverserProxy.inverter(str, 0);
 			System.out.println(i + " " + str);
 			Thread.sleep(5);
 		}
+		time2 = System.nanoTime();
+		System.out.println("Tempo de execucao: " + ((time2-time1)));
 	}
 }
