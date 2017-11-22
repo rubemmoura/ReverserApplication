32f276a93d3e920ad1b2de31e60150f52aabf9db
Rubem Moura <rfmj2@cin.ufpe.br>
2017-11-22 20:56:55 -0300

    adicionando metricas no servidor e proxy

--- Docker/ReverserClient/src/ReverserClient.java
+++ Docker/ReverserClient/src/ReverserClient.java
@@ -10,19 +10,28 @@ public class ReverserClient {
 		
 		// look for Reverser in Naming service
 		ProxyClientProxy clientProxy = new ProxyClientProxy("172.17.0.6", 1315);
-		BindServerProxy reverserProxy = clientProxy.proxy(namingService,0);
-
-		// invoke reverser
-		float time1, time2 = 0;
-
+		BindServerProxy reverserProxy =null;
+		float time1, time2, proxyTime;
+		
 		time1 = System.nanoTime();
+		for(int i = 0; i<1000; i++) {
+			reverserProxy = clientProxy.proxy(namingService,1);
+			Thread.sleep(5);
+		}
+//		time2 = System.nanoTime();
+//		proxyTime = (time2-time1);
+		
+		// invoke reverser
+
+//		time1 = System.nanoTime();
 		StringBuffer str = new StringBuffer("Programacao concorrente Distribuida - Midlleware");
-		for(int i = 0; i<50000; i++) {
-			str = reverserProxy.inverter(str, 0);
+		for(int i = 0; i<1000; i++) {
+			str = reverserProxy.inverter(str,1);
 			System.out.println(i + " " + str);
 			Thread.sleep(5);
 		}
 		time2 = System.nanoTime();
-		System.out.println("Tempo de execucao: " + ((time2-time1)));
+//		System.out.println("Tempo de execucao do proxy: " + proxyTime);
+		System.out.println("Tempo de execucao do inverter: " + ((time2-time1)));
 	}
 }
--- Docker/ServerApplication/src/ReverserServer.java
+++ Docker/ServerApplication/src/ReverserServer.java
@@ -9,10 +9,19 @@ public class ReverserServer {
 		ReverserProxy reverser = new ReverserProxy("172.17.0.4",1314);
 		
 		BindServerProxy bindServiceProxy = new BindServerProxy("172.17.0.3",1316);
-		String serviceName = bindServiceProxy.bind(1, "Reverser", reverser);
 		
+		float time1, time2 = 0;
+		String serviceName = "";
+		time1 = System.nanoTime();
+
+		for(int i=0; i<1000; i++) {
+			serviceName = bindServiceProxy.bind(1, "Reverser" + i, reverser);
+			System.out.println("Servidor da aplicacao " + serviceName + " esta pronto.");
+			Thread.sleep(2);
+		}
 		
-		System.out.println("Servidor da aplicacao " + serviceName + " esta pronto.");
+		time2 = System.nanoTime();
+		System.out.println("Tempo de execucao: " + ((time2-time1)));
 		invoker.invoke(reverser);
 	}
 }
